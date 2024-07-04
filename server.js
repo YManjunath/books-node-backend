5c50012fe1f76af14e7c9ed8b6dca20ea62a75ed
YManjunath <manjuyatnatti@gmail.com>
2024-07-04 19:58:16 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const app = express();
+const port = 3000;
+
+
+app.use(bodyParser.json());
+app.use(cors());
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+    console.log('Connected to Database');
+}, (err) => {
+    console.log('There is problem connecting to database');
+});
+
+// Define Book schema and model
+const bookSchema = new mongoose.Schema({
+    title: String,
+    author: String,
+});
+
+const Book = mongoose.model('Book', bookSchema);
+
+
+app.get('/books', async (req, res, next) => {
+    const book = await Book.find();
+    res.status(200).json({
+        message: 'fetched posts successfully',
+        books: book
+    })
+});
+
+app.get('/books/:id', async (req, res) => {
+    const book = await Book.findById(req.params.id);
+    res.json(book);
+});
+
+
+app.post('/books', async (req, res) => {
+    const newBook = new Book(req.body);
+    await newBook.save();
+    res.json(newBook);
+});
+
+app.put('/books/:id', async (req, res) => {
+    const updatedBook = await Book.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.json(updatedBook);
+});
+
+app.delete('/books/:id', async (req, res) => {
+    await Book.findByIdAndDelete(req.params.id);
+    res.sendStatus(204);
+});
+
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
